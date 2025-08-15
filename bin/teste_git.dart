53742e6728a8e1bf40ceaa6ff7875e06aa11b4c4
Bragantino Neto <rochajogos1@gmail.com>
2025-08-15 16:06:48 -0300

    Commint inicial

---
+++ bin/teste_git.dart
@@ -0,0 +1,5 @@
+import 'package:teste_git/teste_git.dart' as teste_git;
+
+void main(List<String> arguments) {
+  print('Hello world: ${teste_git.calculate()}!');
+}
---
+++ test/teste_git_test.dart
@@ -0,0 +1,8 @@
+import 'package:teste_git/teste_git.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
